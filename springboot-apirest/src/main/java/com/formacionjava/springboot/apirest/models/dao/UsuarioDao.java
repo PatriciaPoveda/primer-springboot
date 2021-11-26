ab06aa47041af23ae745c75b973372906be0ac64
patricia <patricia.poveda-boto@capgemini.com>
2021-11-26 16:47:28 +0100

    tarea 26-11

--- springboot-apirest/src/main/java/com/formacionjava/springboot/apirest/models/dao/UsuarioDao.java
+++ springboot-apirest/src/main/java/com/formacionjava/springboot/apirest/models/dao/UsuarioDao.java
@@ -1,14 +1,14 @@
-package com.formacionjava.springboot.apirest.models.dao;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-import com.formacionjava.springboot.apirest.models.entity.Usuario;
-
-public interface UsuarioDao extends CrudRepository<Usuario, Long> {
-
-	public Usuario findByUsername(String username);
-	
-	@Query("Select u from Usuario u where u.username=?")
-	public Usuario findByUsername2(String username);
-}
+//package com.formacionjava.springboot.apirest.models.dao;
+//
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.formacionjava.springboot.apirest.models.entity.Usuario;
+//
+//public interface UsuarioDao extends CrudRepository<Usuario, Long> {
+//
+//	public Usuario findByUsername(String username);
+//	
+//	@Query("Select u from Usuario u where u.username=?")
+//	public Usuario findByUsername2(String username);
+//}
